f53d7e9da435a93fdb15e6f9f5a05ecd16b740cd
alsomslo <133613694+alsomslo@users.noreply.github.com>
2023-05-25 18:27:34 +0900

    Add files via upload

--- js/main01.js
+++ js/main01.js
@@ -3,8 +3,33 @@ $(function () {
         // arrows: false,
         dots: true,
         autoplay: true,
-        autoplaySpeed: 1000,
-        pauseOnHover: false
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+        speed: 2000
 
     });
+})
+
+$(function () {
+    $('.product_slide').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+
+    $('#product .arrows .left').on('click', function () {
+        $('.product_slide').slick('slickPrev');
+    });
+    $('#product .arrows .right').on('click', function () {
+        $('.product_slide').slick('slickNext');
+    });
+
 })
